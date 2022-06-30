786991247b8d909a05add5264f1637223b719f7f
cicero-elielson <ciceroelielson008@gmail.com>
2022-06-29 21:23:51 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+const nome              = 'cicero' //string
+const sobrenome         = 'elielson' //string
+const data_nascimento   = '01/05/2007' //string
+
+let altura              = 1.70 //number
+let idade               = 15 //number
+let celular             = 18988203220 //number
+
+let celular             = '(18) 98820-3220' //string
+let cor_favorita        = 'vermelho, branco, ciano e roxo' //string
+
+
